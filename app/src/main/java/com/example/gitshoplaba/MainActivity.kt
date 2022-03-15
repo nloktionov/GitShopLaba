9710c3b7ac529d93f89422f89b5b696eb6b15993
nloktionov <nol3000>
2022-03-15 15:28:00 +0300

    bottomNav

--- app/src/main/java/com/example/gitshoplaba/MainActivity.kt
+++ app/src/main/java/com/example/gitshoplaba/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.gitshoplaba
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gitshoplaba.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bagButton.setOnClickListener {
+            binding.bagButtonIcon.setBackgroundColor(Color.RED)
+        }
     }
 }
