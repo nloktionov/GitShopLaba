e89cba0ae04462934861ee3500ae1ce9a581f8a9
nloktionov <nol3000>
2022-03-15 21:26:10 +0300

    LoginScreenMain

---
+++ app/src/main/java/com/example/gitshoplaba/FragmentRegister.kt
@@ -0,0 +1,7 @@
+package com.example.gitshoplaba
+
+import androidx.fragment.app.Fragment
+
+class FragmentRegister : Fragment(R.layout.fragment_register) {
+
+}
--- app/src/main/java/com/example/gitshoplaba/MainActivity.kt
+++ app/src/main/java/com/example/gitshoplaba/MainActivity.kt
@@ -3,17 +3,19 @@ package com.example.gitshoplaba
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 import com.example.gitshoplaba.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        binding.bagButton.setOnClickListener {
-//            binding.bagButtonIcon.setColorFilter(Color.RED)
-//        }
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, FragmentLogin())
+        fragmentTransaction.commit()
     }
 }
